1aa90c72cc84502deaca3c9d712d52cfce3a047c
tapaswi08 <87767842+tapaswi08@users.noreply.github.com>
2023-03-22 17:55:11 +0530

    Update HomeActivity.kt

--- HomeActivity.kt
+++ HomeActivity.kt
@@ -1,49 +1,30 @@
 package com.example.googlemap
-
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.parse.ParseUser
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
 
 class HomeActivity : AppCompatActivity() {
-
-    // creating a variable for our text view
-    private lateinit var userNameTV: TextView
-
-    // button for logout
-    private lateinit var logoutBtn: Button
-
-    @SuppressLint("MissingInflatedId")
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        var recyclerview = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerview.layoutManager = LinearLayoutManager(this)
+        val data = ArrayList<ItemsViewModel>()
 
-        logoutBtn = findViewById(R.id.idBtnLogout)
-
-        // initializing our variables
-        userNameTV = findViewById(R.id.idTVUserName)
-
-        // getting data from intent.
-        val name = intent.getStringExtra("username")
-
-        // setting data to our text view.
-        userNameTV.text = name
-
-        // initializing click listener for logout button
-        logoutBtn.setOnClickListener {
-            // calling a method to logout our user.
-            ParseUser.logOutInBackground { e ->
-                if (e == null) {
-                    Toast.makeText(this, "User Logged Out", Toast.LENGTH_SHORT).show()
-                    val i = Intent(this, LoginActivity::class.java)
-                    startActivity(i)
-                    finish()
-                }
-            }
+        for (i in 1..5) {
+            data.add(ItemsViewModel("User " + i,"Online"))
         }
+        val adapter = MyAdapter(data)
+        recyclerview.adapter = adapter
+        
     }
 }
+
+private fun RecyclerView.layoutManager(linearLayoutManager: LinearLayoutManager) {
+
+}
+
